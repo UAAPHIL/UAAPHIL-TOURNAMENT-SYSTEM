66aefae8ebbc2915951ea67d683823b6058931a1
UAAPHIL <nlusigodo@gmail.com>
2026-08-05 16:25:16 +0800

    Add files via upload

---
+++ tailwind.config_1.ts
@@ -0,0 +1,1 @@
+import type {Config} from "tailwindcss";const config:Config={content:["./src/**/*.{js,ts,jsx,tsx,mdx}"],darkMode:"class",theme:{extend:{colors:{background:"var(--background)",foreground:"var(--foreground)",surface:"var(--surface)","surface-2":"var(--surface-2)",border:"var(--border)",primary:"var(--primary)"}}}};export default config;
